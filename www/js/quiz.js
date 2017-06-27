62a6a0573d32c259656f9f66ebc4ba5d321f4cb6
pouleyKetchoupp <pouleyKetchoup@gmail.com>
2017-06-27 10:52:26 +0900

    quiz answers logic

--- www/js/quiz.js
+++ www/js/quiz.js
@@ -1,14 +1,23 @@
 var quiz_list;
 
 var quiz_started = false;
+var quiz_validated = false;
 
 var quiz_index = 0;
+
+var quiz_score = 0;
 
 var quiz_init = function() {
   console.log("quiz_init");
   
   var quizElement = document.getElementById('quiz_main');
   quizElement.style.visibility='hidden';
+  
+  var quizResultElement = document.getElementById('quiz_result');
+  quizResultElement.style.visibility='hidden';
+  
+  var quizScoreElement = document.getElementById('quiz_score');
+  quizScoreElement.style.visibility='hidden';
 }
 
 var quiz_start = function() {
@@ -32,20 +41,71 @@ var quiz_start = function() {
     
     quiz_started = true;
   }
-  
+    
   quiz_index = -1;
+  
+  quiz_score = 0;
+  quiz_updateScore();
+  
+  var quizResultElement = document.getElementById('quiz_result');
+  quizResultElement.style.visibility='hidden';
+  
+  var quizScoreElement = document.getElementById('quiz_score');
+  quizScoreElement.style.visibility='visible';
+  
   quiz_next();
 }
 
 var quiz_validate = function() {
   console.log("quiz_validate");
   
-  quiz_next();
+  var quizResultElement = document.getElementById('quiz_result');
+  
+  if (quiz_validated) {
+    // Hide result and go next
+    quizResultElement.style.visibility='hidden';
+  
+    quiz_next();
+    
+    if (!quiz_started) {
+      var quizScoreElement = document.getElementById('quiz_score');
+      quizScoreElement.style.visibility='hidden';
+    }
+  }
+  else {
+    // Check and show result
+    var answerElement = document.getElementById('quiz_answer');
+    var answer = answerElement.value;
+    
+    var quizEntry = quiz_list[quiz_index];
+    var correctAnswer = quizEntry.word2;
+    
+    quizResultElement.style.visibility='visible';
+    
+    var quizResultText = document.getElementById('quiz_result_text');
+    if (answer == correctAnswer) {
+      quizResultText.innerText = "Correct!";
+      ++quiz_score;
+    }
+    else {
+      quizResultText.innerText = "Wrong. The answer was: \n" + correctAnswer;
+    }
+    
+    quiz_validated = true;
+    
+    var validateElement = document.getElementById('quiz_validate');
+    if (quiz_index == quiz_list.length - 1) {
+      validateElement.innerText = "Finish";
+    }
+    else {
+      validateElement.innerText = "Next";
+    }
+    
+    quiz_updateScore();
+  }
 }
 
 var quiz_next = function() {
-  console.log("quiz_next");
-  
   if (++quiz_index < quiz_list.length) {
     var quizEntry = quiz_list[quiz_index];
     
@@ -65,6 +125,19 @@ var quiz_next = function() {
     
     quiz_started = false;
   }
+    
+  var validateElement = document.getElementById('quiz_validate');
+  validateElement.innerText = "Validate";
+  
+  quiz_validated = false;
+  
+  var answerElement = document.getElementById('quiz_answer');
+  answerElement.value = null;
+}
+
+var quiz_updateScore = function() {
+  var quizScoreText = document.getElementById('quiz_score_text');
+  quizScoreText.innerText = "Score: " + quiz_score + " / " + (quiz_index + 1);
 }
 
 
